b46ef8deb4ee37c30457c183c5ddb8bf1bda8edc
Rémi GATTAZ <remi.gattaz@gmail.com>
2017-03-08 08:22:39 +0100

    Use ng-repeat to display FAQ and tutos

--- app/scripts/controllers/faq.js
+++ app/scripts/controllers/faq.js
@@ -15,36 +15,23 @@
  */
 angular.module('frontendApp')
 	.controller('FaqCtrl', function ($scope, $http) {
-		/*Affichage des Tutoriels*/
-		function displayTutos() {
-			$http.get('../json/tutos.json').then(function(data) {
-				var tutos = data.data;
-				for(var i = 0; i < tutos.length; i++) {
-					var tutorial = '';
-					tutorial += '<h3>' + tutos[i].name + '</h3>';
-					tutorial += '<p>' + tutos[i].content + '</p>';
-					document.getElementById('tutorials').innerHTML += tutorial;
-				}
-			}, function Error(data) {
-				console.log(data);
-			});
-		}
 
-		/*Affichage de la FAQ*/
-		function displayFaq () {
-			$http.get('../json/faq.json').then(function(data) {
-				var faq = data.data;
-				for(var i = 0; i < faq.length; i++) {
-					var question = '';
-					question += '<h3>' + faq[i].question + '</h3>';
-					question += '<p>' + faq[i].answer + '</p>';
-					document.getElementById('faq').innerHTML += question;
-				}
-			}, function Error(data) {
-				console.log(data);
-			});
-		}
+		var vm = this;
+		this.tutos = [];
+		this.faqs = [];
 
-		displayTutos();
-		displayFaq();
+		/* Récupère les tutos */
+		$http.get('../json/tutos.json').then(function(data) {
+			vm.tutos = data.data;
+		}, function Error(data) {
+			console.log(data);
+		});
+
+		/* Récupère les FAQs */
+		$http.get('../json/faq.json').then(function(data) {
+			vm.faqs = data.data;
+		}, function Error(data) {
+			console.log(data);
+		});
+
 	});
