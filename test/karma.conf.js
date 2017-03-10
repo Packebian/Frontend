7c8e9a1dd799910b191545c5262357acf25f5bc7
Rémi GATTAZ <remi.gattaz@gmail.com>
2017-03-10 09:07:38 +0100

    Update karma configuration

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,8 +29,10 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/auth0.js/build/auth0.js',
-      'bower_components/angular-auth0/angular-auth0.js',
+      'bower_components/angular-jwt/dist/angular-jwt.js',
+      'bower_components/auth0-lock/build/lock.js',
+      'bower_components/angular-lock/angular-lock.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
